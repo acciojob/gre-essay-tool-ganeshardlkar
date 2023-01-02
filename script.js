9f8ff57da0bf3cc2a39cf6c9601e3f8a2feb1262
Ganesh Ardalkar <37583773+ganeshardlkar@users.noreply.github.com>
2023-01-02 19:20:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 //your code here
-function countChars(countfrom,displayto) {
+let evaluatedText = document.getElementById('evaluatedText');
+let wordCount = document.getElementById('wordCount');
+let button = document.getElementById('button');
 
-    var len = document.getElementById(countfrom).value.length;
-    document.getElementById(displayto).textContent = len;
 
+function countWords(){
+    if(evaluatedText.value === ""){
+        console.log("0");
+        wordCount.innerText = 0;
+    }
+    else{
+
+        const arr = evaluatedText.value.trim().split(" ");
+        console.log(evaluatedText.value);
+        if(arr.length > 0){
+            console.log(evaluatedText.value.length);
+            console.log(arr.length);
+            wordCount.innerText = arr.length;
+        }
+    }
 }
